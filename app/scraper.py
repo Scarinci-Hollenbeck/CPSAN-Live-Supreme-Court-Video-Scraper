34eb2c460e770d611c9d524f75fda8e4ce136945
s1100h <scarincihollenbeckweb@gmail.com>
2020-06-04 00:52:21 -0400

    started building out scraper

--- app/scraper.py
+++ app/scraper.py
@@ -4,19 +4,27 @@ cspan_url = 'https://www.c-span.org/search/?sdate=&edate=&searchtype=Videos&sort
 response = get(cspan_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
+# find the video link from video page
+def get_video_link(link):
+  video_response = get(link)
+  video_soup = BeautifulSoup(video_response.text, 'html.parser')
+  video_title = video_soup.find('h3')
+  return video_soup
 
 # scrape the full list of videos
 def get_all_videos():
-  video_list = []
-  return video_list 
+  video_list = soup.find_all('li', {'class': 'onevid'})
 
+  for video in video_list:
+    a = video.find('a')
+    video_link = 'https:' + a['href'], a.get_text()
+    first_video = video_link[0]
+  
+  print(get_video_link(first_video))
+  
 
+get_all_videos()
 # scrape the top video
 def get_latest_video():
   top_video = {}
-  return top_video 
-
-
-# find the video link from video page
-def get_video_link(link):
-  return link
+  return top_video
